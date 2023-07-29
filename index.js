4080737f9eaf05b34e7a409b99a0fb8c3c21afcc
Poncho <ameneses67@gmail.com>
2023-07-29 16:17:21 -0600

    seccion 6

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+
+// Crear la app
+const app = express();
+
+// Routing
+app.use("/", usuarioRoutes);
+
+// Definir un puerto y arrancar el proyecto
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`El servidor está funcionando en el puerto ${port}`);
+});
