da015c8a10308f347366d68e4c6d0733c967035f
Poncho <ameneses67@gmail.com>
2023-08-01 16:49:57 -0600

    orm sequelize

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conexión a la base de datos
+try {
+  await db.authenticate();
+  console.log("Conexión correcta a la base de datos");
+} catch (error) {
+  console.log(error);
+}
 
 // Habilitar pug
 app.set("view engine", "pug");
